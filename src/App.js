45e3c3f8ec20fa5c22667fdc7d7f19f457f5377a
Reena-creator <me230003057@iiti.ac.in>
2025-07-01 01:59:07 +0530

    Added basename = my website

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Router>
+        <Router basename="/my-website">
           <Navbar/>
           <Routes>
             <Route path="/" element={<><Card/><Carousel/><Card2/></>}/>
